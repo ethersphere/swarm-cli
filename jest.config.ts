d3c299863c344e9305288cb0f2d48e1436b7bdf7
nugaon <50576770+nugaon@users.noreply.github.com>
2021-01-08 14:53:11 +0100

    feat: base CLI project setup (#1)
    * feat: base CLI project setup
    
    * fix: add sample files for successful test

---
+++ jest.config.ts
@@ -0,0 +1,38 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+import type { Config } from '@jest/types'
+
+
+export default (): Config.InitialOptions => {
+  return {
+
+    // Indicates whether the coverage information should be collected while executing the test
+    // collectCoverage: false,
+
+    // The directory where Jest should output its coverage files
+    coverageDirectory: 'coverage',
+
+    // An array of regexp pattern strings used to skip coverage collection
+    coveragePathIgnorePatterns: ["/node_modules/"],
+
+    // An array of directory names to be searched recursively up from the requiring module's location
+    moduleDirectories: ["node_modules"],
+
+    // Run tests from one or more projects
+    projects: [
+      {
+        displayName: 'node',
+        testEnvironment: 'node',
+        testRegex: 'test/.*\\.spec\\.ts',
+      },
+    ] as unknown[] as string[], // bad types
+
+    // The root directory that Jest should scan for tests and modules within
+    rootDir: 'test',
+
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    testPathIgnorePatterns: ["/node_modules/"],
+  }
+}
