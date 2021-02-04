725e1e89da3ade527dedf5431e410029d30d205a
nugaon <toth.viktor.levente@gmail.com>
2021-02-04 21:06:59 +0100

    fix: global 'version' option (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import { cli } from 'furious-commander'
+import { cli, Utils } from 'furious-commander'
 import { rootCommandClasses, optionParameters } from './config'
+import PackageJson from '../package.json'
+
+Utils.yargs.version(PackageJson.version)
 
 cli({
   rootCommandClasses,
