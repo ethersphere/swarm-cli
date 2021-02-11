25f2e1d4a2badc701592b610f55d9be1e423a593
Attila Gazso <agazso@gmail.com>
2021-02-11 15:11:43 +0100

    refactor: upload with inheritance

--- src/command/feed/upload.ts
+++ src/command/feed/upload.ts
@@ -22,9 +22,6 @@ export class Upload extends UploadBase {
 
   public readonly name = 'upload'
 
-  @Argument({ key: 'path', describe: 'Path of the file (even directory)', required: true })
-  public path!: string
-
   @Option({ key: 'identity', describe: 'Name of the identity', required: true })
   public identity!: string
 
@@ -33,31 +30,6 @@ export class Upload extends UploadBase {
 
   @Option({ key: 'password', describe: 'Password for the wallet' })
   public password!: string
-
-  @Option({ key: 'pin', type: 'boolean', describe: 'Persist the uploaded data on the gateway node' })
-  public pin!: boolean
-
-  @Option({ key: 'recursive', alias: 'r', describe: 'Upload directory', default: true })
-  public recursive!: boolean
-
-  @Option({ key: 'tag-polling-time', describe: 'Waiting time in ms between tag pollings', default: 500 })
-  public tagPollingTime!: number
-
-  @Option({ key: 'tag-polling-trials', describe: 'After the given trials the tag polling will stop', default: 15 })
-  public tagPollingTrials!: number
-
-  @Option({
-    key: 'index-document',
-    describe: 'Default retrieval file on bzz request without provided filepath',
-    default: 'index.html',
-  })
-  public indexDocument!: string
-
-  @Option({
-    key: 'error-document',
-    describe: 'Default error file on bzz request without with wrong filepath',
-  })
-  public errorDocument!: string
 
   // CLASS FIELDS
 
@@ -71,7 +43,7 @@ export class Upload extends UploadBase {
       exit(1)
     }
     try {
-      await this.upload()
+      await super.run()
 
       const wallet = await getWalletFromIdentity(identity, this.password)
       const signer = wallet.getPrivateKey()
@@ -89,85 +61,4 @@ export class Upload extends UploadBase {
       exit(1)
     }
   }
-
-  public async upload(): Promise<void> {
-    let tag = await this.bee.createTag()
-    let url: string
-
-    if (!FS.existsSync(this.path)) {
-      console.warn(bold().red(`Given filepath '${this.path}' doesn't exist`))
-
-      exit(1)
-    }
-
-    if (FS.lstatSync(this.path).isDirectory()) {
-      console.log('Starting to upload the given folder')
-      console.log(dim('Send data to the Bee node...'))
-
-      if (this.pin) console.log(dim('Pin the uploaded data'))
-
-      this.hash = await this.bee.uploadFilesFromDirectory(this.path, this.recursive, {
-        indexDocument: this.indexDocument,
-        errorDocument: this.errorDocument,
-        tag: tag.uid,
-        pin: this.pin,
-      })
-      url = `${this.beeApiUrl}/bzz/${this.hash}`
-    } else {
-      console.log('Starting to upload the given file')
-      console.log(dim('Send data to the Bee node...'))
-
-      this.hash = await this.bee.uploadFile(FS.createReadStream(this.path), Path.basename(this.path), {
-        tag: tag.uid,
-        pin: this.pin,
-      })
-      url = `${this.beeApiUrl}/files/${this.hash}`
-    }
-    console.log(dim('Data have been sent to the Bee node successfully!'))
-    console.log(bold(`Swarm root hash -> ${green(this.hash)}`))
-
-    console.log(dim('Waiting for file chunks to be synced on Swarm network...'))
-    //refresh tag before populate tracking
-    tag = await this.bee.retrieveTag(tag.uid)
-    const synced = await this.waitForFileSynced(tag)
-
-    if (!synced) return //error message printed before
-
-    console.log(dim('Uploading was successful!'))
-    console.log(bold(`URL -> ${green(url)}`))
-  }
-
-  private async waitForFileSynced(tag: Tag): Promise<boolean> {
-    const tagUid = tag.uid
-    const pollingTime = this.tagPollingTime
-    const pollingTrials = this.tagPollingTrials
-    let synced = false
-    const syncedBar = new SingleBar({}, Presets.rect)
-    console.log('tag', tag) //TODO remove after bug has been presented
-    syncedBar.start(tag.total, 0)
-    for (let i = 0; i < pollingTrials; i++) {
-      tag = await this.bee.retrieveTag(tagUid)
-      const updateState = tag.synced
-
-      syncedBar.update(updateState)
-
-      if (tag.total === updateState) {
-        synced = true
-        break
-      }
-      await sleep(pollingTime)
-    }
-    syncedBar.stop()
-
-    if (!synced) {
-      console.warn(red('Data syncing timeout.'))
-
-      return false
-    } else {
-      console.log(dim('Data has been synced on Swarm network'))
-
-      return true
-    }
-  }
-
 }
