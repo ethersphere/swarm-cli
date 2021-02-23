5ddb5a495cd98e48a4527fe9924987d249fbfc7a
Cafe137 <aron@aronsoos.com>
2021-02-23 11:44:07 +0100

    refactor: split identity export to helper functions

--- src/command/identity/export.ts
+++ src/command/identity/export.ts
@@ -1,4 +1,5 @@
 import { Argument, LeafCommand } from 'furious-commander'
+import { exit } from 'process'
 import { Identity, IdentityType } from '../../service/identity/types'
 import { RootCommand } from '../root-command'
 import { printNoIdentitiesError, promptUserForIdentity } from './common'
@@ -15,48 +16,63 @@ export class Export extends RootCommand implements LeafCommand {
 
   public async run(): Promise<void> {
     this.initCommand()
-
-    if (!this.commandConfig.config.identities) {
-      printNoIdentitiesError(this)
-
-      return
-    }
-
+    this.checkForNoIdentities()
+    const options = this.getV3Identities()
+    this.checkForEmptyIdentities(options)
+    await this.ensureIdentityNameIsProvided(options)
     const { identities } = this.commandConfig.config
-
-    const v3Names: string[] = Object.entries(identities)
-      .filter(identity => identity[1].identityType === IdentityType.v3)
-      .map(identity => identity[0])
-
-    if (!this.identityName) {
-      if (v3Names.length === 0) {
-        this.console.error('You do not have any V3-type identities to choose from')
-
-        return
-      }
-
-      this.identityName = await promptUserForIdentity(v3Names, 'Which identity would you like to export?')
-    }
-
-    if (!identities[this.identityName]) {
-      this.console.error('No such identity')
-
-      return
-    }
-
+    this.checkForExistingIdentity()
     const identity: Identity = identities[this.identityName]
-
-    if (identity.identityType !== IdentityType.v3) {
-      this.console.error('Only V3-type identities can be exported')
-
-      return
-    }
-
+    this.checkForV3Identity(identity)
     this.console.log(JSON.stringify(identity.wallet, null, 4))
   }
 
   /** Init additional properties of class, that are not handled by the CLI framework */
   private initCommand(): void {
     super.init()
+  }
+
+  private getV3Identities(): string[] {
+    return Object.entries(this.commandConfig.config.identities)
+      .filter(identity => identity[1].identityType === IdentityType.v3)
+      .map(identity => identity[0])
+  }
+
+  private async ensureIdentityNameIsProvided(options: string[]): Promise<void> {
+    if (!this.identityName) {
+      this.identityName = await promptUserForIdentity(options, 'Which identity would you like to export?')
+    }
+  }
+
+  private checkForExistingIdentity(): void {
+    if (!this.commandConfig.config.identities[this.identityName]) {
+      this.console.error('No such identity')
+
+      exit(1)
+    }
+  }
+
+  private checkForEmptyIdentities(identities: string[]): void {
+    if (identities.length === 0) {
+      this.console.error('You do not have any V3-type identities to choose from')
+
+      exit(1)
+    }
+  }
+
+  private checkForNoIdentities(): void {
+    if (!this.commandConfig.config.identities) {
+      printNoIdentitiesError(this)
+
+      exit(1)
+    }
+  }
+
+  private checkForV3Identity(identity: Identity): void {
+    if (identity.identityType !== IdentityType.v3) {
+      this.console.error('Only V3-type identities can be exported')
+
+      exit(1)
+    }
   }
 }
