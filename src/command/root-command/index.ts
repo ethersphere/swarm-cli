f8cf440668528fe226c34bd9b7e9173412bf0924
Cafe137 <aron@aronsoos.com>
2021-04-26 14:02:25 +0200

    refactor: simplify root init

--- src/command/root-command/index.ts
+++ src/command/root-command/index.ts
@@ -1,17 +1,9 @@
 import { Bee, BeeDebug } from '@ethersphere/bee-js'
 import { ExternalOption } from 'furious-commander'
-import { IOption } from 'furious-commander/dist/option'
-import { beeApiUrl, configFolder, quiet, verbose } from '../../config'
 import { CommandConfig } from './command-config'
 import { CommandLog, VerbosityLevel } from './command-log'
 
-/**
- * This class can be parent of the Commands to handle root options of the CLI
- */
 export class RootCommand {
-  // CLI FIELDS
-
-  /** API URL of Bee */
   @ExternalOption('bee-api-url')
   public beeApiUrl!: string
 
@@ -30,52 +22,23 @@ export class RootCommand {
   @ExternalOption('quiet')
   public quiet!: boolean
 
-  // CLASS FIELDS
-
-  public appName = 'swarm-cli'
-
   public bee!: Bee
-
   public beeDebug!: BeeDebug
-
+  public console!: CommandLog
+  public appName = 'swarm-cli'
   public commandConfig!: CommandConfig
 
-  public console!: CommandLog
-
-  /**
-   * Init Root command fields
-   *
-   * if BEE_API_URL environment variable has been set the CLI will use that connection string
-   */
   protected init(): void {
-    // Console logs of default commands
+    this.bee = new Bee(this.beeApiUrl)
+    this.beeDebug = new BeeDebug(this.beeDebugApiUrl)
+    this.commandConfig = new CommandConfig(this.appName, this.console, this.configFolder)
     this.verbosity = VerbosityLevel.Normal
 
-    if (this.optionPassed(quiet)) {
+    if (this.quiet) {
       this.verbosity = VerbosityLevel.Quiet
-    }
-
-    if (this.optionPassed(verbose)) {
+    } else if (this.verbose) {
       this.verbosity = VerbosityLevel.Verbose
     }
     this.console = new CommandLog(this.verbosity)
-
-    // CLI Configuration
-    if (!this.optionPassed(configFolder)) {
-      if (process.env.SWARM_CLI_CONFIG_FOLDER) this.configFolder = process.env.SWARM_CLI_CONFIG_FOLDER
-    }
-    this.commandConfig = new CommandConfig(this.appName, this.console, this.configFolder)
-
-    if (!this.optionPassed(beeApiUrl)) {
-      if (process.env.BEE_API_URL) this.beeApiUrl = process.env.BEE_API_URL
-      else if (this.commandConfig.config.beeApiUrl) this.beeApiUrl = this.commandConfig.config.beeApiUrl
-    } // else it gets its default option value
-
-    this.bee = new Bee(this.beeApiUrl)
-    this.beeDebug = new BeeDebug(this.beeDebugApiUrl)
-  }
-
-  protected optionPassed(option: IOption): boolean {
-    return process.argv.includes(`--${option.key}`) || process.argv.includes(`-${option.alias}`)
   }
 }
