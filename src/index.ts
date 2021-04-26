5f12a3669482f348b47ddb59f8606f5e09825efb
Cafe137 <aron@aronsoos.com>
2021-04-26 14:09:00 +0200

    refactor: remove yargs version and help

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import { cli, Utils } from 'furious-commander'
-import PackageJson from '../package.json'
+import { cli } from 'furious-commander'
 import { optionParameters, rootCommandClasses } from './config'
-
-Utils.yargs.alias('V', 'version')
-Utils.yargs.alias('h', 'help')
-Utils.yargs.version(PackageJson.version)
 
 cli({
   rootCommandClasses,
