9d1c4272bc47541e59f46f0e3406f08c4c542ebc
Cafe137 <aron@aronsoos.com>
2021-05-05 12:23:19 +0200

    feat: add pinning superclass to check for gateway

---
+++ src/command/pinning/pinning-command.ts
@@ -0,0 +1,16 @@
+import { exit } from 'process'
+import { isGateway } from '../../utils'
+import { RootCommand } from '../root-command'
+
+export class PinningCommand extends RootCommand {
+  protected init(): void {
+    super.init()
+
+    if (isGateway(this.beeApiUrl)) {
+      this.console.error('Pinning is currently not supported on the gateway node.')
+      this.console.error('You can use the pinning API with your local Bee node.')
+
+      exit(1)
+    }
+  }
+}
