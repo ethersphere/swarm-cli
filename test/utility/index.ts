2be08b9f5ae0c1e56303e21f079755c6a297573e
Cafe137 <aron@aronsoos.com>
2021-05-25 11:08:57 +0200

    refactor: add cli return type

--- test/utility/index.ts
+++ test/utility/index.ts
@@ -1,7 +1,7 @@
 import { cli } from 'furious-commander'
 import { optionParameters, rootCommandClasses } from '../../src/config'
 
-export async function invokeTestCli(argv: string[]) {
+export async function invokeTestCli(argv: string[]): ReturnType<typeof cli> {
   const commandBuilder = await cli({
     rootCommandClasses,
     optionParameters,
