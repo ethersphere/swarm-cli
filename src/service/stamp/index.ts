9c9ffb3c809a2961e57c481316138c80a10c1404
Cafe137 <aron@aronsoos.com>
2021-05-27 19:30:27 +0200

    docs: write JSDoc for pickStamp

--- src/service/stamp/index.ts
+++ src/service/stamp/index.ts
@@ -2,6 +2,16 @@ import { Bee } from '@ethersphere/bee-js'
 import { exit } from 'process'
 import { CommandLog } from '../../command/root-command/command-log'
 
+/**
+ * Displays an interactive stamp picker to select a Stamp ID.
+ *
+ * A typical use case is to prompt the user for a stamp, when
+ * the command requires one, but was not specified in `argv`.
+ *
+ * Makes a request via `bee` to fetch possible stamps.
+ *
+ * @returns {Promise<string>} Hex representation of the Stamp ID.
+ */
 export async function pickStamp(bee: Bee, console: CommandLog): Promise<string> {
   const stamps = await bee.getAllPostageBatch()
 
