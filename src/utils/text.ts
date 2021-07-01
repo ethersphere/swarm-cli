adaa1f21fb192847a0e09127ceef4fd2d626c0c6
Cafe137 <aron@aronsoos.com>
2021-07-01 13:26:36 +0200

    refactor: clean up prompts

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,5 +1,17 @@
 import { bold, green } from 'kleur'
 
+function goUpOneRow(): string {
+  return '\u001b[1A'
+}
+
+function deleteWholeRow(): string {
+  return '\u001b[2K'
+}
+
+export function deletePreviousLine(): void {
+  process.stdout.write('\r' + goUpOneRow() + deleteWholeRow())
+}
+
 export function createKeyValue(key: string, value: string | number | boolean, padLength?: number): string {
   return `${green(bold(key + ':')).padEnd(padLength ? padLength + 1 : 0)} ${bold(String(value))}`
 }
