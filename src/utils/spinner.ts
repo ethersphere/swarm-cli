318090d1acad732bbe669f405edabe048f250cd8
Cafe137 <aron@aronsoos.com>
2021-07-02 14:11:02 +0200

    refactor: add static spinner for windows

--- src/utils/spinner.ts
+++ src/utils/spinner.ts
@@ -1,7 +1,11 @@
 import ora from 'ora'
+import { platform } from 'os'
 
-const frames = ['⬡ ⬡ ⬡', '⬢ ⬡ ⬡', '⬡ ⬢ ⬡', '⬡ ⬡ ⬢']
+const isWindows = platform() === 'win32'
+
+const frames = isWindows ? ['...'] : ['⬡ ⬡ ⬡', '⬢ ⬡ ⬡', '⬡ ⬢ ⬡', '⬡ ⬡ ⬢']
+const interval = isWindows ? 999_999_999 : 300
 
 export function createSpinner(text: string): ora.Ora {
-  return ora({ text, interval: 300, spinner: { frames } })
+  return ora({ text, interval, spinner: { frames } })
 }
