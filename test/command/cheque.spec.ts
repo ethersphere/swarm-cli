f4c566994bc42c8ceb3017ff1a63ecf0085c8cee
Cafe137 <aron@aronsoos.com>
2021-07-02 09:16:51 +0200

    test(refactor): change toBe to toContain

--- test/command/cheque.spec.ts
+++ test/command/cheque.spec.ts
@@ -1,5 +1,4 @@
 import { Server } from 'http'
-import { bold, green } from 'kleur'
 import { createChequeMockHttpServer } from '../http-mock/cheque-mock'
 import { describeCommand, invokeTestCli } from '../utility'
 
@@ -37,7 +36,8 @@ describeCommand(
     it('should print cheques', async () => {
       process.env.BEE_DEBUG_API_URL = 'http://localhost:1377'
       await invokeTestCli(['cheque', 'list'])
-      expect(getLastMessage()).toBe(bold('Cheque Value: ') + '8944000000000 PLUR')
+      expect(getLastMessage()).toContain('Cheque Value:')
+      expect(getLastMessage()).toContain('8944000000000 PLUR')
     })
 
     it('should not print cheques when --minimum is higher', async () => {
@@ -75,9 +75,8 @@ describeCommand(
     it('should allow specifying gas price and limit for cashout', async () => {
       process.env.BEE_DEBUG_API_URL = 'http://localhost:1377'
       await invokeTestCli(['cheque', 'cashout', '--all', '--gas-price', '100', '--gas-limit', '100'])
-      expect(getLastMessage()).toBe(
-        green(bold('Tx:           ')) + '0x11df9811dc8caaa1ff4389503f2493a8c46b30c0a0b5f8aa54adbb965374c0ae',
-      )
+      expect(getLastMessage()).toContain('Tx:')
+      expect(getLastMessage()).toContain('0x11df9811dc8caaa1ff4389503f2493a8c46b30c0a0b5f8aa54adbb965374c0ae')
     })
 
     it('should not cashout any cheques when --minimum is higher', async () => {
@@ -94,13 +93,12 @@ describeCommand(
         '--peer',
         '1105536d0f270ecaa9e6e4347e687d1a1afbde7b534354dfd7050d66b3c0faad',
       ])
-      expect(getNthLastMessage(3)).toBe(
-        bold('Peer Address: ') + '1105536d0f270ecaa9e6e4347e687d1a1afbde7b534354dfd7050d66b3c0faad',
-      )
-      expect(getNthLastMessage(2)).toBe(bold('Cheque Value: ') + '8944000000000 PLUR')
-      expect(getLastMessage()).toBe(
-        green(bold('Tx:           ')) + '0x11df9811dc8caaa1ff4389503f2493a8c46b30c0a0b5f8aa54adbb965374c0ae',
-      )
+      expect(getNthLastMessage(3)).toContain('Peer Address:')
+      expect(getNthLastMessage(3)).toContain('1105536d0f270ecaa9e6e4347e687d1a1afbde7b534354dfd7050d66b3c0faad')
+      expect(getNthLastMessage(2)).toContain('Cheque Value:')
+      expect(getNthLastMessage(2)).toContain('8944000000000 PLUR')
+      expect(getLastMessage()).toContain('Tx:')
+      expect(getLastMessage()).toContain('0x11df9811dc8caaa1ff4389503f2493a8c46b30c0a0b5f8aa54adbb965374c0ae')
     })
 
     it('should raise error when withdrawing negative amount', async () => {
