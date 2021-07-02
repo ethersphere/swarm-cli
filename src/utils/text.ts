8e143ae3a55b8a37fa38d4dfb1b2fa209d84e738
Cafe137 <aron@aronsoos.com>
2021-07-02 10:32:49 +0200

    refactor: remove boldness from values

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -13,5 +13,5 @@ export function deletePreviousLine(): void {
 }
 
 export function createKeyValue(key: string, value: string | number | boolean, padLength?: number): string {
-  return `${green(bold(key + ':')).padEnd(padLength ? padLength + 1 : 0)} ${bold(String(value))}`
+  return `${green(bold(key + ':')).padEnd(padLength ? padLength + 1 : 0)} ${String(value)}`
 }
