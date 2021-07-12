b482d0dd5eaae1b08288845d2a1ece5194aa5fd5
Cafe137 <aron@aronsoos.com>
2021-07-12 13:46:24 +0200

    fix: print more info in verbose mode after stamp buy

--- src/command/stamp/stamp-command.ts
+++ src/command/stamp/stamp-command.ts
@@ -1,9 +1,3 @@
-import { createKeyValue } from '../../utils/text'
 import { RootCommand } from '../root-command'
 
-export class StampCommand extends RootCommand {
-  protected printBatchId(batchId: string): void {
-    this.console.log(createKeyValue('Stamp ID', batchId))
-    this.console.quiet(batchId)
-  }
-}
+export class StampCommand extends RootCommand {}
