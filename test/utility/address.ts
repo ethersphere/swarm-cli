02b13a75e2fd761aff4ec8ca87dce4589d271538
Cafe137 <aron@aronsoos.com>
2021-07-14 19:43:21 +0200

    test: use correct pss addresses to avoid 'wants self' test errors

---
+++ test/utility/address.ts
@@ -0,0 +1,24 @@
+import { invokeTestCli } from '.'
+import { Addresses } from '../../src/command/addresses'
+
+export async function getPssAddress(beeDebugApiUrl: string): Promise<string> {
+  const execution = await invokeTestCli(['stamp', 'addresses', '--bee-debug-api-url', beeDebugApiUrl])
+
+  return (execution.runnable as Addresses).nodeAddresses.pssPublicKey
+}
+
+export function getQueenPssAddress(stringLength: number): string {
+  if (!process.env.QUEEN_PSS_ADDRESS) {
+    throw Error('Queen PSS address is not set.')
+  }
+
+  return process.env.QUEEN_PSS_ADDRESS.slice(0, stringLength)
+}
+
+export function getWorkerPssAddress(stringLength: number): string {
+  if (!process.env.WORKER_PSS_ADDRESS) {
+    throw Error('Worker PSS address is not set.')
+  }
+
+  return process.env.WORKER_PSS_ADDRESS.slice(0, stringLength)
+}
