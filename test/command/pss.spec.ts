453a0f5ae2db3f583a490b058139cf33a057a745
Cafe137 <aron@aronsoos.com>
2021-07-23 11:31:37 +0200

    test(pss): add more send and receive test cases

--- test/command/pss.spec.ts
+++ test/command/pss.spec.ts
@@ -5,19 +5,41 @@ import { describeCommand, invokeTestCli } from '../utility'
 import { getWorkerPssAddress } from '../utility/address'
 import { getStampOption } from '../utility/stamp'
 
+async function callReceive(extraArgs: string[]): Promise<Receive> {
+  const invocation = invokeTestCli([
+    'pss',
+    'receive',
+    '--topic-string',
+    'PSS Test',
+    '--bee-api-url',
+    'http://localhost:11633',
+    '--timeout',
+    '30000',
+    ...extraArgs,
+  ])
+  const receive = (await invocation).runnable as Receive
+  await sleep(3000)
+
+  return receive
+}
+
+async function callSend(message: string): Promise<void> {
+  await invokeTestCli([
+    'pss',
+    'send',
+    '-T',
+    'PSS Test',
+    '--target',
+    getWorkerPssAddress(4),
+    '--message',
+    message,
+    ...getStampOption(),
+  ])
+}
+
 describeCommand('Test PSS command', ({ getNthLastMessage, getLastMessage }) => {
   it('should receive sent pss message', async () => {
-    const invocation = invokeTestCli([
-      'pss',
-      'receive',
-      '--topic-string',
-      'PSS Test',
-      '--bee-api-url',
-      'http://localhost:11633',
-      '--timeout',
-      '10000',
-    ])
-    await sleep(1000)
+    const receive = await callReceive([])
     await invokeTestCli([
       'pss',
       'send',
@@ -29,7 +51,6 @@ describeCommand('Test PSS command', ({ getNthLastMessage, getLastMessage }) => {
       'Bzzz Bzzzz Bzzzz',
       ...getStampOption(),
     ])
-    const receive: Receive = (await invocation).runnable as Receive
     expect(receive.receivedMessage).toBe('Bzzz Bzzzz Bzzzz')
   })
 
@@ -38,19 +59,7 @@ describeCommand('Test PSS command', ({ getNthLastMessage, getLastMessage }) => {
       unlinkSync('test/testconfig/out.txt')
     }
     writeFileSync('test/testconfig/in.txt', 'Message in a file')
-    invokeTestCli([
-      'pss',
-      'receive',
-      '--topic-string',
-      'PSS Test',
-      '--bee-api-url',
-      'http://localhost:11633',
-      '--timeout',
-      '10000',
-      '--out-file',
-      'test/testconfig/out.txt',
-    ])
-    await sleep(1000)
+    await callReceive(['--out-file', 'test/testconfig/out.txt'])
     await invokeTestCli([
       'pss',
       'send',
@@ -62,7 +71,7 @@ describeCommand('Test PSS command', ({ getNthLastMessage, getLastMessage }) => {
       'test/testconfig/in.txt',
       ...getStampOption(),
     ])
-    await sleep(1000)
+    await sleep(4000)
     expect(existsSync('test/testconfig/out.txt')).toBeTruthy()
     const messageFromFile = readFileSync('test/testconfig/out.txt', 'ascii')
     expect(messageFromFile).toBe('Message in a file')
@@ -104,64 +113,42 @@ describeCommand('Test PSS command', ({ getNthLastMessage, getLastMessage }) => {
   })
 
   it('should not allow sending payload above 4000 bytes', async () => {
-    await invokeTestCli([
-      'pss',
-      'send',
-      '-T',
-      'PSS Test',
-      '--target',
-      getWorkerPssAddress(4),
-      '--message',
-      '0'.repeat(4001),
-      ...getStampOption(),
-    ])
+    await callSend('0'.repeat(4001))
     expect(getNthLastMessage(2)).toContain('Maximum payload size is 4000 bytes.')
     expect(getLastMessage()).toContain('You tried sending 4001 bytes.')
   })
 
   it('should allow sending payload of 4000 bytes', async () => {
-    await invokeTestCli([
-      'pss',
-      'send',
-      '-T',
-      'PSS Test',
-      '--target',
-      getWorkerPssAddress(4),
-      '--message',
-      '0'.repeat(4000),
-      ...getStampOption(),
-    ])
+    await callSend('0'.repeat(4000))
     expect(getLastMessage()).toContain('Message sent successfully.')
   })
 
   it('should not allow sending multibyte payload above 4000 bytes', async () => {
-    await invokeTestCli([
-      'pss',
-      'send',
-      '-T',
-      'PSS Test',
-      '--target',
-      getWorkerPssAddress(4),
-      '--message',
-      '😃'.repeat(1001),
-      ...getStampOption(),
-    ])
+    await callSend('😃'.repeat(1001))
     expect(getNthLastMessage(2)).toContain('Maximum payload size is 4000 bytes.')
     expect(getLastMessage()).toContain('You tried sending 4004 bytes.')
   })
 
   it('should allow sending multibyte payload of 4000 bytes', async () => {
-    await invokeTestCli([
-      'pss',
-      'send',
-      '-T',
-      'PSS Test',
-      '--target',
-      getWorkerPssAddress(4),
-      '--message',
-      '😃'.repeat(1000),
-      ...getStampOption(),
-    ])
+    await callSend('😃'.repeat(1000))
     expect(getLastMessage()).toContain('Message sent successfully.')
+  })
+
+  it('should receive multibyte data correctly', async () => {
+    const receive = await callReceive([])
+    await callSend('🐝🐝🐝')
+    expect(receive.receivedMessage).toBe('🐝🐝🐝')
+  })
+
+  it('should receive zero bytes correctly', async () => {
+    const receive = await callReceive([])
+    await callSend(Buffer.from([0, 0, 0, 0]).toString('ascii'))
+    expect(receive.receivedMessage).toBe('\x00\x00\x00\x00')
+  })
+
+  it('should receive ascii text correctly', async () => {
+    const receive = await callReceive([])
+    await callSend('A honey bee, a busy, flying insect that lives in a hive and makes honey.')
+    expect(receive.receivedMessage).toBe('A honey bee, a busy, flying insect that lives in a hive and makes honey.')
   })
 })
