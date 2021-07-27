f8b1959d9c06565c5f494b6e1b0ea1a9a73ce698
Cafe137 <aron@aronsoos.com>
2021-07-27 15:05:00 +0200

    refactor: simplify node setup

--- test/utility/stamp.ts
+++ test/utility/stamp.ts
@@ -1,8 +1,8 @@
 import { invokeTestCli } from '.'
 import { Buy } from '../../src/command/stamp/buy'
 
-export const buyStamp = async (beeApiUrl = 'http://localhost:1633'): Promise<string> => {
-  const execution = await invokeTestCli(['stamp', 'buy', '--depth', '20', '--amount', '1', '--bee-api-url', beeApiUrl])
+export const buyStamp = async (): Promise<string> => {
+  const execution = await invokeTestCli(['stamp', 'buy', '--depth', '20', '--amount', '1'])
   const command = execution.runnable as Buy
 
   return command.postageBatchId
