e01aad9bcfac6a735e23b1ed7ca7c1331b542e66
Cafe137 <aron@aronsoos.com>
2021-09-28 10:42:59 +0200

    test: add commands for tests

--- src/command/manifest/index.ts
+++ src/command/manifest/index.ts
@@ -1,10 +1,15 @@
 import { GroupCommand } from 'furious-commander'
+import { Add } from './add'
+import { Create } from './create'
 import { Download } from './download'
 import { List } from './list'
+import { Merge } from './merge'
+import { Remove } from './remove'
+import { Sync } from './sync'
 
 export class Manifest implements GroupCommand {
   public readonly name = 'manifest'
   public readonly description = 'Operate on manifests'
 
-  public subCommandClasses = [Download, List]
+  public subCommandClasses = [Create, Add, Sync, Merge, Remove, Download, List]
 }
