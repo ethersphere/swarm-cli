b6cef03ece9e96864cbbfcc8220d10e325d4d1de
Cafe137 <aron@aronsoos.com>
2021-10-26 11:37:03 +0200

    refactor: improve top level download command with aggregation

--- src/command/download.ts
+++ src/command/download.ts
@@ -1,8 +1,9 @@
 import { Data } from '@ethersphere/bee-js'
 import { writeFileSync } from 'fs'
-import { Argument, LeafCommand, Option } from 'furious-commander'
+import { Aggregation, LeafCommand, Option } from 'furious-commander'
 import { loadAllNodes, MantarayNode } from 'mantaray-js'
-import { extendObject, referenceToHex } from '../utils'
+import { referenceToHex } from '../utils'
+import { BzzAddress } from '../utils/bzz-address'
 import { Download as ManifestDownload } from './manifest/download'
 import { RootCommand } from './root-command'
 
@@ -11,40 +12,42 @@ export class Download extends RootCommand implements LeafCommand {
   public readonly aliases = ['dl']
   public readonly description = 'Download arbitrary Swarm hash'
 
-  @Argument({ key: 'hash', description: 'Swarm reference', required: true })
-  public hash!: string
+  @Aggregation(['manifest download'])
+  public manifestDownload!: ManifestDownload
 
-  @Option({ key: 'save', type: 'boolean', description: 'Save to file' })
-  public save!: boolean
+  @Option({ key: 'stdout', type: 'boolean', description: 'Print to stdout (single files only)' })
+  public stdout!: boolean
+
+  private address!: BzzAddress
 
   public async run(): Promise<void> {
     await super.init()
 
+    this.address = new BzzAddress(this.manifestDownload.bzzUrl)
+
     if (await this.isManifest()) {
-      const manifestDownload = new ManifestDownload()
-      extendObject(manifestDownload as unknown as Record<string, unknown>, this as unknown as Record<string, unknown>)
-      manifestDownload.bzzUrl = this.hash
-      await manifestDownload.run()
+      this.console.log('Given address is a manifest - downloading...')
+      await this.manifestDownload.run()
     } else {
       await this.downloadFile()
     }
   }
 
   private async downloadFile(): Promise<void> {
-    const response = await this.bee.downloadFile(this.hash)
+    const response = await this.bee.downloadFile(this.address.hash)
     const { name, data } = response
 
-    if (this.save) {
-      const path = name || this.hash
-      writeFileSync(path, data)
-    } else {
+    if (this.stdout) {
       process.stdout.write(data)
+    } else {
+      const path = this.manifestDownload.destination || name || this.address.hash
+      writeFileSync(path, data)
     }
   }
 
   private async isManifest(): Promise<boolean> {
     try {
-      const response = await this.bee.downloadData(this.hash)
+      const response = await this.bee.downloadData(this.address.hash)
       const node = new MantarayNode()
       node.deserialize(response)
       await loadAllNodes(this.load.bind(this), node)
