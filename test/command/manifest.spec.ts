d695315343a8ff872eac75537a7e7258566235fe
Cafe137 <aron@aronsoos.com>
2021-10-26 13:03:13 +0200

    test: clear console messages before asserting

--- test/command/manifest.spec.ts
+++ test/command/manifest.spec.ts
@@ -305,6 +305,7 @@ describeCommand('Test Upload command', ({ consoleMessages, hasMessageContaining 
     const invocation = await invokeTestCli(['upload', 'test/testpage', ...getStampOption()])
     const { hash } = invocation.runnable as Upload
     await invokeTestCli(['manifest', 'download', hash])
+    consoleMessages.length = 0
     expect(consoleMessages[0]).toContain('images/swarm.png')
     expect(consoleMessages[1]).toContain('index.html')
     expect(consoleMessages[2]).toContain('swarm.bzz')
