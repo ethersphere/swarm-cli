ef7d36f399c8ec04c8a453c2385b9c2b057f7b4f
Cafe137 <aron@aronsoos.com>
2022-02-03 12:04:02 +0100

    feat: add --out-file to identity export

--- src/command/identity/export.ts
+++ src/command/identity/export.ts
@@ -1,4 +1,5 @@
-import { Argument, LeafCommand } from 'furious-commander'
+import { writeFileSync } from 'fs'
+import { Argument, LeafCommand, Option } from 'furious-commander'
 import { isSimpleWallet } from '../../service/identity'
 import { IdentityType } from '../../service/identity/types'
 import { CommandLineError } from '../../utils/error'
@@ -12,16 +13,32 @@ export class Export extends IdentityCommand implements LeafCommand {
   @Argument({ key: 'name', description: 'Name of the identity to be exported' })
   public identityName!: string
 
+  @Option({
+    key: 'out-file',
+    alias: 'o',
+    description: 'Export identity to file',
+  })
+  public outFile!: string
+
   public async run(): Promise<void> {
     await super.init()
     const { identity } = await this.getOrPickIdentity(this.identityName)
 
     if (identity.identityType === IdentityType.v3) {
-      this.console.log(JSON.stringify(identity.wallet, null, 4))
+      this.writeIdentityString(JSON.stringify(identity.wallet, null, 4))
     } else if (isSimpleWallet(identity.wallet, identity.identityType)) {
-      this.console.log(identity.wallet.privateKey)
+      this.writeIdentityString(identity.wallet.privateKey)
     } else {
       throw new CommandLineError('Unsupported identity type')
+    }
+  }
+
+  private writeIdentityString(identity: string): void {
+    if (this.outFile) {
+      writeFileSync(this.outFile, identity)
+    } else {
+      this.console.log(identity)
+      this.console.quiet(identity)
     }
   }
 }
