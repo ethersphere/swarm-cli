47e8b51492c2412b7f122f37fd9b6576e332805c
Cafe137 <xcafe137@gmail.com>
2022-08-09 13:42:17 +0200

    build: remove manifest-js

--- src/utils/bzz-address.ts
+++ src/utils/bzz-address.ts
@@ -1,6 +1,8 @@
 import { Bee } from '@ethersphere/bee-js'
-import { ManifestJs } from '@ethersphere/manifest-js'
+import { MantarayFork, MantarayNode, MetadataMapping } from 'mantaray-js'
 import { CommandLineError } from './error'
+
+const INDEX_DOCUMENT_FORK_PREFIX = '47'
 
 export class BzzAddress {
   public hash: string
@@ -28,11 +30,58 @@ export class BzzAddress {
 export async function makeBzzAddress(bee: Bee, url: string): Promise<BzzAddress> {
   const address = new BzzAddress(url)
 
-  const feedReference = await new ManifestJs(bee).resolveFeedManifest(address.hash)
+  const feedReference = await resolveFeedManifest(bee, address.hash)
 
   if (feedReference) {
     address.hash = feedReference
   }
 
   return address
+}
+
+async function resolveFeedManifest(bee: Bee, hash: string): Promise<string | null> {
+  const metadata = await getRootSlashMetadata(bee, hash)
+
+  if (!metadata) {
+    return null
+  }
+
+  const owner = metadata['swarm-feed-owner']
+  const topic = metadata['swarm-feed-topic']
+
+  if (!owner || !topic) {
+    return null
+  }
+
+  const reader = bee.makeFeedReader('sequence', topic, owner)
+  const response = await reader.download()
+
+  return response.reference
+}
+
+async function getRootSlashMetadata(bee: Bee, hash: string): Promise<MetadataMapping | null> {
+  const data = await bee.downloadData(hash)
+  const node = new MantarayNode()
+  node.deserialize(data)
+
+  if (!node.forks) {
+    return null
+  }
+  const fork = Reflect.get(node.forks, INDEX_DOCUMENT_FORK_PREFIX) as MantarayFork | undefined
+
+  if (!fork) {
+    return null
+  }
+  const metadataNode = fork.node
+
+  if (!metadataNode.IsWithMetadataType()) {
+    return null
+  }
+  const metadata = metadataNode.getMetadata
+
+  if (!metadata) {
+    return null
+  }
+
+  return metadata
 }
