b040aa95ea29f80a342876f3aefa72da4f6f9398
Luis Ricardo Sanchez <luis.sanchez@esferasoluciones.com>
2022-08-16 08:50:54 -0500

    fix: convert TTL to human readable format (#395)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -51,6 +51,20 @@ export function getByteSize(data: string | Uint8Array): number {
   return Buffer.byteLength(data, 'utf-8')
 }
 
+export function secondsToDhms(secs: number): string {
+  const d = Math.floor(secs / (3600 * 24))
+  const h = Math.floor((secs % (3600 * 24)) / 3600)
+  const m = Math.floor((secs % 3600) / 60)
+  const s = Math.floor(secs % 60)
+
+  const dDisplay = d > 0 ? d + (d === 1 ? ' day, ' : ' days, ') : ''
+  const hDisplay = h > 0 ? h + (h === 1 ? ' hour, ' : ' hours, ') : ''
+  const mDisplay = m > 0 ? m + (m === 1 ? ' minute, ' : ' minutes, ') : ''
+  const sDisplay = s > 0 ? s + (s === 1 ? ' second' : ' seconds') : ''
+
+  return dDisplay + hDisplay + mDisplay + sDisplay
+}
+
 /**
  * Lists all files recursively in a folder
  * @param path folder path
