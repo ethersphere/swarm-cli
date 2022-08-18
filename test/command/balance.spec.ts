d133e8a2f6b3534850812eea4978a4a7fb396b29
Cafe137 <xcafe137@gmail.com>
2022-08-18 15:05:08 +0200

    test(fix): pick open port

--- test/command/balance.spec.ts
+++ test/command/balance.spec.ts
@@ -2,14 +2,14 @@ import { createChequeMockHttpServer } from '../http-mock/cheque-mock'
 import { describeCommand, invokeTestCli } from '../utility'
 
 describeCommand('Test Status command', ({ consoleMessages }) => {
-  const server = createChequeMockHttpServer(1377)
+  const server = createChequeMockHttpServer(1379)
 
   afterAll(() => {
     server.close()
   })
 
   it('should print balance', async () => {
-    process.env.BEE_DEBUG_API_URL = 'http://localhost:1377'
+    process.env.BEE_DEBUG_API_URL = 'http://localhost:1379'
     await invokeTestCli(['balance'])
     expect(consoleMessages[1]).toContain('Total')
     expect(consoleMessages[1]).toContain('BZZ')
