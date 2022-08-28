31df827f3508bb17eef5f503faa0ab5a1463d32e
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2022-08-28 18:50:26 +0200

    fix: do not print empty label (#406)
    * fix: do not print empty label
    
    * test: test for stamp label
    
    * test(fix): add missing --yes flag

--- test/command/stamp.spec.ts
+++ test/command/stamp.spec.ts
@@ -9,16 +9,14 @@ describeCommand(
     it('should list stamps', async () => {
       await invokeTestCli(['stamp', 'list'])
       expect(consoleMessages[0]).toContain('Stamp ID:')
-      expect(consoleMessages[1]).toContain('Label: ')
-      expect(consoleMessages[2]).toContain('Usage:')
+      expect(consoleMessages[1]).toContain('Usage:')
     })
 
     it('should show a specific stamp', async () => {
       await invokeTestCli(['stamp', 'show', process.env.STAMP || ''])
       expect(consoleMessages[0]).toContain('Stamp ID:')
       expect(consoleMessages[0]).toContain(process.env.STAMP)
-      expect(consoleMessages[1]).toContain('Label: ')
-      expect(consoleMessages[2]).toContain('Usage:')
+      expect(consoleMessages[1]).toContain('Usage:')
     })
 
     it('should not allow buying stamp with amount 0', async () => {
@@ -77,9 +75,11 @@ describeCommand(
         '--depth',
         '20',
         '--amount',
-        '1',
+        '59k',
         '--wait-usable',
         '--yes',
+        '--label',
+        'Alice',
       ])
       const command = execution.runnable as Buy
       expect(command.yes).toBe(true)
@@ -93,6 +93,7 @@ describeCommand(
       expect(getNthLastMessage(9)).toContain('Usage')
       expect(getNthLastMessage(9)).toContain('0%')
       expect(getNthLastMessage(10)).toContain('Label:')
+      expect(getNthLastMessage(10)).toContain('Alice')
       expect(getNthLastMessage(11)).toContain('Stamp ID')
       expect(getNthLastMessage(11)).toContain(id)
     })
