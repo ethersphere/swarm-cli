480c83efbb600af62b45e8fb694a02b2a7cd125b
Aleksandr Ivanov <aivanov@oneinc.com>
2023-03-09 14:54:44 -0600

    fix: #435 update bee-js version

--- src/command/balance.ts
+++ src/command/balance.ts
@@ -16,15 +16,15 @@ export class Balance extends RootCommand implements LeafCommand {
     await super.init()
 
     this.console.dim('Looking up balance...')
-    const { bzz, xDai } = await this.beeDebug.getWalletBalance()
+    const { bzzBalance, nativeTokenBalance } = await this.beeDebug.getWalletBalance()
     const { totalBalance, availableBalance } = await this.beeDebug.getChequebookBalance()
 
     const PLURConvertionRate = BigNumber(10).pow(16)
     const ETHConvertionRate = BigNumber(10).pow(18)
     const totalBalanceBZZ = BigNumber(totalBalance).dividedBy(PLURConvertionRate)
     const availableBalanceBZZ = BigNumber(availableBalance).dividedBy(PLURConvertionRate)
-    const walletBZZ = BigNumber(bzz).dividedBy(PLURConvertionRate)
-    const walletDAI = BigNumber(xDai).dividedBy(ETHConvertionRate)
+    const walletBZZ = BigNumber(bzzBalance).dividedBy(PLURConvertionRate)
+    const walletDAI = BigNumber(nativeTokenBalance).dividedBy(ETHConvertionRate)
     this.console.log(chalk.bold('Node wallet'))
     this.console.log(createKeyValue('BZZ', toSignificantDigits(walletBZZ)))
     this.console.log(createKeyValue('DAI', toSignificantDigits(walletDAI)))
