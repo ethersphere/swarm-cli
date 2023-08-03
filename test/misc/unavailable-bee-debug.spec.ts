88defe5700371a333f3bd0c8949da71b8eb5902d
Cafe137 <xcafe137@gmail.com>
2023-08-03 19:26:44 +0200

    test: remove dead test

--- test/misc/unavailable-bee-debug.spec.ts
+++ test/misc/unavailable-bee-debug.spec.ts
@@ -2,13 +2,6 @@ import { describeCommand, invokeTestCli } from '../utility'
 import { getStampOption } from '../utility/stamp'
 
 describeCommand('Test unavailable bee-debug', ({ consoleMessages, hasMessageContaining }) => {
-  it('should fail for cheque commands (mandatory)', async () => {
-    await invokeTestCli(['balance', '--bee-debug-api-url', 'http://localhost:3362'])
-    expect(consoleMessages[0]).toContain('Could not reach Debug API at http://localhost:3362')
-    expect(consoleMessages[1]).toContain('Make sure you have the Debug API enabled in your Bee config')
-    expect(consoleMessages[2]).toContain('or correct the URL with the --bee-debug-api-url option.')
-  })
-
   it('should fail for stamp commands (mandatory)', async () => {
     await invokeTestCli(['stamp', 'list', '--bee-debug-api-url', 'http://localhost:3362'])
     expect(consoleMessages[0]).toContain('Could not reach Debug API at http://localhost:3362')
