d63bdac6d309103467dbd77feafee14f5b9dcd13
Ferenc Sárai <ferenc.sarai@solarpunk.buzz>
2024-06-05 09:25:34 +0200

    feat: add patch command to grantee group

--- src/command/grantee/get.ts
+++ src/command/grantee/get.ts
@@ -1,20 +1,22 @@
 import { Argument, LeafCommand } from 'furious-commander'
 import { GranteeCommand } from './grantee-command'
+import { createKeyValue } from '../../utils/text'
 
 export class Get extends GranteeCommand implements LeafCommand {
   public readonly name = 'get'
   public readonly description = 'Get grantee list'
 
   @Argument({
-    key: 'path',
-    description: 'Path to the file with grantee list',
+    key: 'reference',
+    description: 'Grantee list reference',
     required: true,
-    autocompletePath: true,
     conflicts: 'stdin',
   })
-  public path!: string
+  public reference!: string
 
   public async run(): Promise<void> {
     await super.init()
+    const response = await this.bee.getGrantees(this.reference)
+    this.console.log(createKeyValue('Grantee public keys', response.data.join('\n')))
   }
 }
--- src/command/grantee/index.ts
+++ src/command/grantee/index.ts
@@ -1,11 +1,12 @@
 import { GroupCommand } from 'furious-commander'
 import { Add } from './add'
 import { Get } from './get'
+import { Patch } from './patch'
 
 export class Grantee implements GroupCommand {
   public readonly name = 'grantee'
 
-  public readonly description = 'Add, Get grantee list'
+  public readonly description = 'Add, Get, Patch grantee list'
 
-  public subCommandClasses = [Add, Get]
+  public subCommandClasses = [Add, Get, Patch]
 }
