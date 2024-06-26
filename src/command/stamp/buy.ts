2b02ad6d92315e42918f662d47674e17cb4c26a3
Cafe137 <xcafe137@gmail.com>
2024-06-26 17:28:49 +0200

    fix: set immutable stamps as default

--- src/command/stamp/buy.ts
+++ src/command/stamp/buy.ts
@@ -39,7 +39,7 @@ export class Buy extends StampCommand implements LeafCommand {
   })
   public gasPrice!: bigint
 
-  @Option({ key: 'immutable', description: 'Disable stamp reuse', type: 'boolean' })
+  @Option({ key: 'immutable', description: 'Disable stamp reuse', type: 'boolean', default: true })
   public immutable!: boolean
 
   @Option({ key: 'label', description: 'Label of the postage stamp' })
