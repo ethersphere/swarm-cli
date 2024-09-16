754ac0ca61fd87f7579ca9d865b74664b1918b10
Cafe137 <xcafe137@gmail.com>
2024-09-16 15:46:20 +0200

    fix: remove indices from curl tests

--- test/misc/curl.spec.ts
+++ test/misc/curl.spec.ts
@@ -1,43 +1,57 @@
+import { toMatchLinesInOrder } from '../custom-matcher'
 import { describeCommand, invokeTestCli } from '../utility'
 import { getStampOption } from '../utility/stamp'
+
+expect.extend({
+  toMatchLinesInOrder,
+})
 
 describeCommand('--curl flag', ({ consoleMessages }) => {
   it('should print upload command without encryption', async () => {
     await invokeTestCli(['upload', 'test/testpage/index.html', '--curl', ...getStampOption()])
-    expect(consoleMessages[1]).toContain('curl -X POST "http://localhost:1633/bzz?name=index.html" ')
-    expect(consoleMessages[1]).toContain(`-H "swarm-postage-batch-id: ${getStampOption()[1]}`)
-    expect(consoleMessages[1]).not.toContain('swarm-encrypt')
+    expect(consoleMessages).toMatchLinesInOrder([
+      [
+        'curl -X POST "http://localhost:1633/bzz?name=index.html" ',
+        `-H "swarm-postage-batch-id: ${getStampOption()[1]}`,
+        '-H "swarm-encrypt: true"',
+      ],
+    ])
+    expect(consoleMessages.every(x => !x.includes('swarm-encrypt'))).toBeTruthy()
   })
 
   it('should print upload command with encryption', async () => {
     await invokeTestCli(['upload', 'test/testpage/index.html', '--encrypt', '--curl', ...getStampOption()])
-    expect(consoleMessages[1]).toContain('curl -X POST "http://localhost:1633/bzz?name=index.html" ')
-    expect(consoleMessages[1]).toContain(`-H "swarm-postage-batch-id: ${getStampOption()[1]}`)
-    expect(consoleMessages[1]).toContain('-H "swarm-encrypt: true"')
+    expect(consoleMessages).toMatchLinesInOrder([
+      [
+        'curl -X POST "http://localhost:1633/bzz?name=index.html" ',
+        `-H "swarm-postage-batch-id: ${getStampOption()[1]}`,
+        '-H "swarm-encrypt: true"',
+      ],
+    ])
   })
 
   it('should print file', async () => {
     await invokeTestCli(['upload', 'test/testpage/index.html', '--curl', ...getStampOption()])
-    expect(consoleMessages[1]).toContain('curl -X POST "http://localhost:1633/bzz?name=index.html" ')
-    expect(consoleMessages[1]).toContain('--data @test/testpage/index.html')
+    expect(consoleMessages).toMatchLinesInOrder([
+      ['curl -X POST "http://localhost:1633/bzz?name=index.html" ', '--data @test/testpage/index.html'],
+    ])
   })
 
   it('should print <buffer> for directories', async () => {
     await invokeTestCli(['upload', 'test/testpage/', '--curl', ...getStampOption()])
-    expect(consoleMessages[2]).toContain('curl -X POST "http://localhost:1633/bzz" ')
-    expect(consoleMessages[2]).toContain('--data <buffer>')
+    expect(consoleMessages).toMatchLinesInOrder([['curl -X POST "http://localhost:1633/bzz"', '--data <buffer>']])
   })
 
   it('should not print undefined params', async () => {
     await invokeTestCli(['upload', 'test/testpage/index.html', '--curl', '--drop-name', ...getStampOption()])
-    expect(consoleMessages[1]).toContain('curl -X POST "http://localhost:1633/bzz" ')
-    expect(consoleMessages[1]).toContain('--data @test/testpage/index.html')
+    expect(consoleMessages).toMatchLinesInOrder([
+      ['curl -X POST "http://localhost:1633/bzz"', '--data @test/testpage/index.html'],
+    ])
   })
 
   it('should detect content type', async () => {
     await invokeTestCli(['upload', 'test/testpage/index.html', '--curl', ...getStampOption()])
-    expect(consoleMessages[1]).toContain('-H "content-type: text/html"')
-    expect(consoleMessages[1]).not.toContain('Content-Type')
+    expect(consoleMessages).toMatchLinesInOrder([['-H "content-type: text/html"']])
   })
 
   it('should use custom content type', async () => {
@@ -49,13 +63,11 @@ describeCommand('--curl flag', ({ consoleMessages }) => {
       '--curl',
       ...getStampOption(),
     ])
-    expect(consoleMessages[1]).toContain('-H "content-type: swarm/bzz"')
-    expect(consoleMessages[1]).not.toContain('Content-Type')
+    expect(consoleMessages).toMatchLinesInOrder([['-H "content-type: swarm/bzz"']])
   })
 
   it('should fall back with undetectable content type', async () => {
     await invokeTestCli(['upload', 'test/testpage/swarm.bzz', '--curl', ...getStampOption()])
-    expect(consoleMessages[1]).toContain('-H "content-type: application/octet-stream"')
-    expect(consoleMessages[1]).not.toContain('Content-Type')
+    expect(consoleMessages).toMatchLinesInOrder([['-H "content-type: application/octet-stream"']])
   })
 })
