da34318b65731bbb2eadabb83470b7586b973427
Cafe137 <xcafe137@gmail.com>
2024-09-16 16:11:50 +0200

    fix: pass arg first

--- test/prompt/feed-prompt.spec.ts
+++ test/prompt/feed-prompt.spec.ts
@@ -20,40 +20,40 @@ describeCommand(
 
     it('feed upload should prompt for identity when it is misspelled', async () => {
       jest.spyOn(inquirer, 'prompt').mockClear().mockResolvedValueOnce({ value: 'main' })
-      await invokeTestCli(['feed', 'upload', '-v', 'README.md', '-i', '_main', '-P', 'secret', ...getStampOption()])
+      await invokeTestCli(['feed', 'upload', 'README.md', '-v', '-i', '_main', '-P', 'secret', ...getStampOption()])
       expect(inquirer.prompt).toHaveBeenCalledTimes(1)
       expect(getLastMessage()).toContain('Successfully uploaded to feed.')
     })
 
     it('feed upload should prompt for password when it is not given', async () => {
       jest.spyOn(inquirer, 'prompt').mockClear().mockResolvedValueOnce({ value: 'secret' })
-      await invokeTestCli(['feed', 'upload', '-v', 'README.md', '-i', 'main', ...getStampOption()])
+      await invokeTestCli(['feed', 'upload', 'README.md', '-v', '-i', 'main', ...getStampOption()])
       expect(inquirer.prompt).toHaveBeenCalledTimes(1)
       expect(getLastMessage()).toContain('Successfully uploaded to feed.')
     })
 
     it('feed upload should fail when running in quiet mode and stamp is missing', async () => {
-      await invokeTestCli(['feed', 'upload', '-q', 'README.md', '-i', 'main', '-P', 'secret'])
+      await invokeTestCli(['feed', 'upload', 'README.md', '-q', '-i', 'main', '-P', 'secret'])
       expect(getLastMessage()).toContain('Required option not provided: --stamp')
     })
 
     it('feed upload should fail when running in quiet mode and identity is missing', async () => {
-      await invokeTestCli(['feed', 'upload', '-q', 'README.md', '-P', 'secret', ...getStampOption()])
+      await invokeTestCli(['feed', 'upload', 'README.md', '-q', '-P', 'secret', ...getStampOption()])
       expect(getLastMessage()).toContain('Required option not provided: --identity')
     })
 
     it('feed upload should fail when running in quiet mode and identity is misspelled', async () => {
-      await invokeTestCli(['feed', 'upload', '-q', 'README.md', '-i', '_main', '-P', 'secret', ...getStampOption()])
+      await invokeTestCli(['feed', 'upload', 'README.md', '-q', '-i', '_main', '-P', 'secret', ...getStampOption()])
       expect(getNthLastMessage(4)).toContain('The provided identity does not exist.')
     })
 
     it('feed upload should fail when running in quiet mode and password is wrong', async () => {
-      await invokeTestCli(['feed', 'upload', '-q', 'README.md', '-i', 'main', '-P', '_secret', ...getStampOption()])
+      await invokeTestCli(['feed', 'upload', 'README.md', '-q', '-i', 'main', '-P', '_secret', ...getStampOption()])
       expect(getNthLastMessage(3)).toContain('Key derivation failed - possibly wrong passphrase')
     })
 
     it('feed upload should fail when running in quiet mode and password is missing', async () => {
-      await invokeTestCli(['feed', 'upload', '-q', 'README.md', '-i', 'main', ...getStampOption()])
+      await invokeTestCli(['feed', 'upload', 'README.md', '-q', '-i', 'main', ...getStampOption()])
       expect(consoleMessages[0]).toContain('Password must be passed with the --password option in quiet mode')
     })
   },
