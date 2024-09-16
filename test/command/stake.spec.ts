389524cedca5a978a6d7c3ee8d319e028f400900
Cafe137 <xcafe137@gmail.com>
2024-09-16 17:11:30 +0200

    fix: fix tests

--- test/command/stake.spec.ts
+++ test/command/stake.spec.ts
@@ -9,9 +9,9 @@ describeCommand('Test Stake command', ({ consoleMessages }) => {
   it('should print balance in quiet mode', async () => {
     await invokeTestCli(['stake', '--quiet'])
 
-    const initialStake = parseFloat(consoleMessages[0])
+    const initialStake = parseFloat(consoleMessages[0].split(' ')[1])
     await invokeTestCli(['stake', '--quiet', '--deposit', '100_000T'])
-    const afterDepositStake = parseFloat(consoleMessages[1])
+    const afterDepositStake = parseFloat(consoleMessages[1].split(' ')[1])
     expect(afterDepositStake).toBeGreaterThan(initialStake)
   })
 })
--- test/prompt/stamp-prompt.spec.ts
+++ test/prompt/stamp-prompt.spec.ts
@@ -8,7 +8,7 @@ describeCommand('Postage stamp price estimation prompt', ({ consoleMessages }) =
     await invokeTestCli(['stamp', 'buy', '--depth', '24', '--amount', '596046400'])
     expect(consoleMessages[0]).toBe('Estimated cost: 1.000 xBZZ')
     expect(consoleMessages[1]).toBe('Estimated capacity: 64.00 GB')
-    expect(consoleMessages[2]).toBe('Estimated TTL: 1 day 10 hours 29 minutes 36 seconds')
+    expect(consoleMessages[2]).toBe('Estimated TTL: 34 hours')
     expect(inquirer.prompt).toHaveBeenCalledWith({
       message: 'Confirm the purchase',
       name: 'value',
