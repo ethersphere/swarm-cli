152a4519dfd7a84fa0ca946d8c48d795348f5d00
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2025-05-07 12:57:06 +0200

    feat: update to new batch effective utilization table (#588)
    * feat: update to new batch effective utilization table
    
    * fix: adjust estimated capacity

--- test/prompt/stamp-prompt.spec.ts
+++ test/prompt/stamp-prompt.spec.ts
@@ -7,7 +7,7 @@ describeCommand('Postage stamp price estimation prompt', ({ consoleMessages }) =
     jest.spyOn(inquirer, 'prompt').mockResolvedValueOnce({ value: 'n' })
     await invokeTestCli(['stamp', 'buy', '--depth', '24', '--amount', '596046400'])
     expect(consoleMessages[0]).toBe('Estimated cost: 0.9999999198822400 xBZZ')
-    expect(consoleMessages[1]).toBe('Estimated capacity: 41.171 GB')
+    expect(consoleMessages[1]).toBe('Estimated capacity: 40.084 GB')
     expect(consoleMessages[2]).toBe('Estimated TTL: 34 hours')
     expect(inquirer.prompt).toHaveBeenCalledWith({
       message: 'Confirm the purchase',
