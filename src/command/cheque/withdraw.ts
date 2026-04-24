f51489f41e917559e97c3d738cee3307a7946be6
Gergely Békési <gergely.bekesi@ethswarm.org>
2026-04-24 09:56:47 +0200

    feat: add unit flag for cheque deposit and withraw

--- src/command/cheque/withdraw.ts
+++ src/command/cheque/withdraw.ts
@@ -1,6 +1,8 @@
-import { Argument, LeafCommand } from 'furious-commander'
-import { createKeyValue } from '../../utils/text'
-import { ChequeCommand } from './cheque-command'
+import { Argument, LeafCommand, Option } from 'furious-commander'
+import { createKeyValue, errorText } from '../../utils/text'
+import { ChequeCommand, VALID_UNITS } from './cheque-command'
+import { exit } from 'process'
+import { BZZ } from '@ethersphere/bee-js'
 
 export class Withdraw extends ChequeCommand implements LeafCommand {
   public readonly name = 'withdraw'
@@ -18,11 +20,31 @@ export class Withdraw extends ChequeCommand implements LeafCommand {
   })
   public amount!: bigint
 
+  @Option({
+    key: 'unit',
+    type: 'string',
+    description: 'Unit of the amount',
+    required: true,
+    default: 'bzz',
+  })
+  public unit!: string
+
   public async run(): Promise<void> {
     super.init()
 
-    const response = await this.bee.withdrawTokens(this.amount.toString())
+    this.validateUnit()
+    const amountInPlur =
+      this.unit === 'bzz' ? BZZ.fromDecimalString(this.amount.toString()).toPLURBigInt() : this.amount
+
+    const response = await this.bee.withdrawTokens(amountInPlur.toString())
     this.console.log(createKeyValue('Tx', response.toHex()))
     this.console.quiet(response.toHex())
+  }
+
+  private validateUnit() {
+    if (!VALID_UNITS.includes(this.unit)) {
+      this.console.error(errorText(`Invalid unit '${this.unit}'. Valid units are: ${VALID_UNITS.join(', ')}`))
+      exit(1)
+    }
   }
 }
