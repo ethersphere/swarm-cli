dd9a74b6977d426a659e0ab65f89d0ff189d7673
Gergely Békési <slapec93@users.noreply.github.com>
2026-04-28 08:31:25 +0200

    feat: add upload history enable, disable and status commands (#683)
    * feat: add enable method for history
    
    * feat: respect history config and add disable command
    
    * feat: status command for history
    
    * fix: add command file
    
    * test: update test coverage
    
    * feat: add specs for enable
    
    * feat: cover disable and status commands with spec
    
    * test: update test coverage
    
    * fix: improve coverage
    
    * test: update test coverage
    
    * test: update test coverage
    
    * test: update test coverage
    
    * test: update test coverage
    
    * feat: show entry count instead of file size
    
    * test: update test coverage
    
    * fix: update history file delete logic
    
    * fix: delete debug log
    
    * test: update test coverage
    
    * fix: update stamp prompt spec
    
    * test: update test coverage
    
    * fix: handle edge-cases
    
    * test: update test coverage
    
    ---------
    
    Co-authored-by: Gergely Békési <gergely.bekesi@ethswarm.org>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ test/prompt/history-prompt.spec.ts
@@ -0,0 +1,46 @@
+import inquirer from 'inquirer'
+import { describeCommand, invokeTestCli } from '../utility'
+import { existsSync } from 'fs'
+import { getStampOption } from '../utility/stamp'
+import chalk from 'chalk'
+
+async function uploadTestFile() {
+  const uploadFilePath = `${__dirname}/../testpage/images/swarm.png`
+  await invokeTestCli(['upload', uploadFilePath, ...getStampOption()])
+}
+
+describeCommand(
+  'Using History Disable Command with Prompts',
+  ({ consoleMessages, configFolderPath, getLastMessage }) => {
+    it('history disables asks whether the file should be deleted', async () => {
+      await invokeTestCli(['history', 'enable'])
+      await uploadTestFile()
+      // user presses 'n' to not delete the history file
+      jest.spyOn(inquirer, 'prompt').mockClear().mockResolvedValueOnce({ value: false })
+      await invokeTestCli(['history', 'disable'])
+
+      expect(consoleMessages[6]).toEqual('Upload history tracking disabled')
+      expect(existsSync(`${configFolderPath}/upload-history.json`)).toEqual(true)
+      expect(inquirer.prompt).toHaveBeenCalledWith({
+        message: 'Do you want to delete the upload history file? This action cannot be undone.',
+        name: 'value',
+        prefix: chalk.bold.cyan('?'),
+        type: 'confirm',
+      })
+      await invokeTestCli(['history', 'disable', '--yes'])
+    })
+
+    describe('when history tracking is re-enabled', () => {
+      it('should not overwrite existing history file', async () => {
+        await invokeTestCli(['history', 'enable'])
+        await uploadTestFile()
+        jest.spyOn(inquirer, 'prompt').mockClear().mockResolvedValueOnce({ value: false })
+        await invokeTestCli(['history', 'disable'])
+        await invokeTestCli(['history', 'enable'])
+        await invokeTestCli(['history', 'status'])
+        expect(getLastMessage()).toEqual('Number of history entries: 1')
+        await invokeTestCli(['history', 'disable', '--yes'])
+      })
+    })
+  },
+)
--- test/prompt/stamp-prompt.spec.ts
+++ test/prompt/stamp-prompt.spec.ts
@@ -1,14 +1,15 @@
 import chalk from 'chalk'
 import inquirer from 'inquirer'
 import { describeCommand, invokeTestCli } from '../utility'
+import { createKeyValue } from '../../src/utils/text'
 
 describeCommand('Postage stamp price estimation prompt', ({ consoleMessages }) => {
   it('stamp buy should prompt for price confirmation', async () => {
-    jest.spyOn(inquirer, 'prompt').mockResolvedValueOnce({ value: 'n' })
+    jest.spyOn(inquirer, 'prompt').mockResolvedValueOnce({ value: false })
     await invokeTestCli(['stamp', 'buy', '--depth', '24', '--amount', '596046400'])
-    expect(consoleMessages[0]).toBe('Estimated cost: 0.9999999198822400 xBZZ')
-    expect(consoleMessages[1]).toBe('Estimated capacity: 40.084 GB')
-    expect(consoleMessages[2]).toBe('Estimated TTL: 34 hours')
+    expect(consoleMessages[0]).toBe(createKeyValue('Estimated cost', '0.9999999198822400 xBZZ'))
+    expect(consoleMessages[1]).toBe(createKeyValue('Estimated capacity', '40.084 GB'))
+    expect(consoleMessages[2]).toBe(createKeyValue('Estimated TTL', '34 hours'))
     expect(inquirer.prompt).toHaveBeenCalledWith({
       message: 'Confirm the purchase',
       name: 'value',
