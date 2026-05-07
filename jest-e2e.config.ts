d1a171c007f722913ebe5647ba32462664c9d311
Gergely Békési <gergely.bekesi@ethswarm.org>
2026-05-07 10:22:25 +0200

    test: fire up a bee node for e2e environment

--- jest-e2e.config.ts
+++ jest-e2e.config.ts
@@ -3,9 +3,26 @@
  * https://jestjs.io/docs/en/configuration.html
  */
 import type { Config } from '@jest/types'
-import { Dates } from 'cafe-utility'
+import { Dates, System } from 'cafe-utility'
+import { CommandLog, VerbosityLevel } from './src/command/root-command/command-log'
+import { Bee } from '@ethersphere/bee-js'
 
-export default (): Config.InitialOptions => {
+export default async (): Promise<Config.InitialOptions> => {
+  const port = 1633
+  const bee = new Bee(`http://localhost:${port}`)
+  const console = new CommandLog(VerbosityLevel.Normal)
+
+  const startedAt = Date.now()
+  console.log('Waiting for Bee node to warm up on port', port)
+
+  await System.waitFor(async () => (await bee.getStatus()).isWarmingUp === false, {
+    attempts: 300,
+    waitMillis: Dates.seconds(1),
+    requiredConsecutivePasses: 3,
+  })
+  const elapsed = Date.now() - startedAt
+  console.log(`Bee node on port ${port} warmed up in ${elapsed} milliseconds`)
+
   return {
     collectCoverage: false,
     forceExit: true,
