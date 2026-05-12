5684b2040a72c1ee18a379c0115711f42a70ee00
Cafe137 <xcafe137@gmail.com>
2026-05-12 21:34:15 +0200

    test: add create-batch test

---
+++ test/command/create-batch.spec.ts
@@ -0,0 +1,29 @@
+import { Strings } from 'cafe-utility'
+import { describeCommand, invokeTestCli } from '../utility'
+
+describeCommand('Test `utility create-batch` command', ({ consoleMessages }) => {
+  it('should create batch', async () => {
+    await invokeTestCli([
+      'utility',
+      'create-batch',
+      '--private-key',
+      '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d',
+      '--depth',
+      '17',
+      '--amount',
+      '10B',
+      '--json-rpc-url',
+      'http:s://localhost:8545',
+      '--yes',
+    ])
+    expect(
+      Strings.linesMatchInOrder(consoleMessages, [
+        ['Approving spending of', 'BZZ to'],
+        ['Waiting 3 blocks on approval tx'],
+        ['Creating postage batch for', 'with depth 17 and amount 100000000000'],
+        ['Waiting 3 blocks on create batch tx'],
+        ['Batch created with ID'],
+      ]),
+    ).toBe(true)
+  })
+})
