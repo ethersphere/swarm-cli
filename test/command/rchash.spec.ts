b1fa0b1ded206732475e5536bd30ab2c5758ce4f
Cafe137 <xcafe137@gmail.com>
2026-05-13 15:04:01 +0200

    test: rename utility.spec.ts to rchash.spec.ts and improve output validation

--- test/command/rchash.spec.ts
+++ test/command/rchash.spec.ts
@@ -1,13 +1,18 @@
+import { toMatchLinesInOrder } from '../custom-matcher'
 import { describeCommand, invokeTestCli } from '../utility'
+
+expect.extend({
+  toMatchLinesInOrder,
+})
 
 describeCommand('Test Utility rchash command', ({ consoleMessages }) => {
   it('should print reserve sampling duration', async () => {
     await invokeTestCli(['utility', 'rchash'])
-    expect(consoleMessages.find(m => m.includes('Reserve sampling duration'))).toBeTruthy()
+    expect(consoleMessages).toMatchLinesInOrder([['Reserve sampling duration']])
   })
 
   it('should print reserve sampling duration with custom depth', async () => {
     await invokeTestCli(['utility', 'rchash', '--depth', '2'])
-    expect(consoleMessages.find(m => m.includes('Reserve sampling duration'))).toBeTruthy()
+    expect(consoleMessages).toMatchLinesInOrder([['Reserve sampling duration']])
   })
 })
