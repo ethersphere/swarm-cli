16b642cf87fb34ac1d2b08647df4a3874e40d686
Gergely Békési <gergely.bekesi@ethswarm.org>
2026-05-26 12:44:23 +0200

    fix: update specs

--- test/command/redeem.spec.ts
+++ test/command/redeem.spec.ts
@@ -1,5 +1,5 @@
 import { Wallet } from 'ethers'
-import { sendBzzTransaction, sendNativeTransaction } from '../../src/utils/rpc'
+import { mintBzzTransaction, sendNativeTransaction } from '../../src/utils/rpc'
 import { toMatchLinesInOrder } from '../custom-matcher'
 import { describeCommand, invokeTestCli } from '../utility'
 
@@ -7,21 +7,21 @@ expect.extend({
   toMatchLinesInOrder,
 })
 
-const FUNDER_KEY = '0x566058308ad5fa3888173c741a1fb902c9f1f19559b11fc2738dfc53637ce4e9'
-const JSON_RPC_URL = 'http://localhost:9545'
+const FUNDER_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+const JSON_RPC_URL = 'http://localhost:8545'
 const ETH_01 = 100_000_000_000_000_000n.toString()
 const BZZ_5 = 50_000_000_000_000_000n.toString()
 
 describeCommand('Test `utility redeem` command', ({ consoleMessages }) => {
   it('should redeem funds to target address', async () => {
-    process.env.SWARM_CLI_NETWORK_ID = '4020'
-    process.env.SWARM_CLI_BZZ_ADDRESS = '0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab'
+    process.env.SWARM_CLI_NETWORK_ID = '1337'
+    process.env.SWARM_CLI_BZZ_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 
     const sourceWallet = Wallet.createRandom()
     const targetWallet = Wallet.createRandom()
 
     await sendNativeTransaction(FUNDER_KEY, sourceWallet.address, ETH_01, JSON_RPC_URL)
-    await sendBzzTransaction(FUNDER_KEY, sourceWallet.address, BZZ_5, JSON_RPC_URL)
+    await mintBzzTransaction(FUNDER_KEY, sourceWallet.address, BZZ_5, JSON_RPC_URL)
 
     await invokeTestCli([
       'utility',
